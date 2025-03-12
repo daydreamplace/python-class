ef4c451900629b58331bc535ba5b4eb6bab41fb4
Eden <you8inpark@gmail.com>
2025-03-12 17:43:00 +0900

    feat: 2주차 자료형 실습

--- datatype.py
+++ datatype.py
@@ -6,7 +6,38 @@
 # print(a)
 
 # ---------------------
-a = 9
-b = 4
-c = a // b
-print(c)
+# a = 9.3
+# b = 4.7
+# c = a % b
+# print(c)
+
+# ---------------------
+# a = 'Python'
+# print(a)
+# b = '123' # 문자열임
+# print(b)
+# c = "Python's"
+# print(c)
+
+# ---------------------
+# multiline = "Life is too short. You need python"
+# multiline = "Life is too short.\nYou need python"
+# multiline = '''Life is too short.
+# You need python'''
+# multiline = """
+# "Life" is too short.
+# You ne 's       ed python
+# """
+# multiline = 123
+# print(multiline)
+# """
+# ㅇㄹ아룅머린엄
+# 아러ㅣ;dkfjlal
+# dkfjdljfldjf
+# """
+
+# ---------------------
+head = "Python"
+tail = " is         fun!"
+result = head * 2
+print(result)
