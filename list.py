fafe0892d4fef6b2cc6dfe27b4cf805939fff3a6
Eden <you8inpark@gmail.com>
2025-03-19 17:23:51 +0900

    3주차 자료형 실습

---
+++ list.py
@@ -0,0 +1,10 @@
+a = [1, 2, 3]
+print(a[1]) # 2
+print(a[0] + a[2]) # 4
+print(a[-1]) # 3
+
+del a[1]
+print(a)
+
+b = a + [4, 5]
+print(b)
---
+++ python_text.py
@@ -0,0 +1,83 @@
+# a = "=" * 50
+
+# print(a)
+# print('ptptptp')
+# print(a)
+
+
+# # ---------------------
+# a = "Life is too short, You need Python"
+# b = a[0] + a[1] + a[2] + a[3] # Life
+# b = a[0:4] # Life
+# b = a[0:5] # Life
+# b = a[0:6] # Life i
+# b = a[19] # Y
+# b = a[19:] # You need Python
+# b = a[:17] # Life is too short
+# b = a[19:-2] # You need Pyth
+# print(b)
+
+# # ---------------------
+# a = "20230331Rainy"
+# date = a[:8]
+# weather = a[8:]
+# print(date) # 20230331
+# print(weather) # Rainy
+
+# # ---------------------
+# a = "20230331Rainy"
+# year = a[:4]
+# day = a[4:8]
+# weather = a[:8]
+# print(year)
+# print(day)
+# print(weather)
+
+# # ---------------------
+# a = "Pithon"
+# b = a[:1] + "y" + a[2:]
+# a[1] = "y"
+# print(b)
+
+# # ---------------------
+# a = "I eat %d apples." % 3.32
+# a = "I eat %s apples." % "five"
+
+# number = 3
+# text = "abe"
+# a = "I eat %s apples %d." % (text, number)
+# print(a)
+
+# # ---------------------
+# number = 10
+# day = "three"
+# a = "I ate %d apples. so I was sick for %s days." % (number, day)
+# print(a) 
+
+# # ---------------------
+# a = "%10s" % "hi"
+# print(a)
+# a = "%-10sjane" % "hi"
+# print(a)
+
+# a = "%0.4f" % 3.42134234
+# print(a)
+# b = "%10.4f" % 3.42134234
+# print(b)
+
+# # ---------------------
+# a = 'i eat {0} apples'.format(3)
+# print(a)
+
+# a = 'i eat {0} apples'.format('five')
+# print(a)
+
+# number = 3
+# a = 'i eat {0} apples'.format(number)
+# print(a)
+
+# # ---------------------
+# number = 10
+# day = "three"
+# a = "I ate {number} apples. So I was sick for {day} days.".format(number = 3, day = 10)
+# print(a)
