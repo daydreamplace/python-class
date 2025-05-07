e7e13f036a4c59fdad0dc671fb938ed7f90ce517
Eden <you8inpark@gmail.com>
2025-05-07 16:36:16 +0900

    모듈 사용해보기

---
+++ 0507모듈/module01.py
@@ -0,0 +1,21 @@
+# 1, 2, 3.
+# def add(a, b):
+#     return a + b
+
+# def sub(a, b):
+#     return a - b
+
+# if __name__ == "__main__":
+#     print(add(13, 5))
+#     print(sub(13, 5))
+
+
+# 4. 
+PI = 3.141592
+
+class Math:
+    def solve(self, r):
+        return PI * (r ** 2)
+
+def add(a, b):
+    return a + b
---
+++ 0507모듈/module02.py
@@ -0,0 +1,25 @@
+# 1.
+# import module01
+
+# print(module01.add(3, 5))
+
+# 2.
+# # module01에 있는 add, sub 함수 가져오기
+# from module01 import add, sub
+# # module01에 있는 모든 함수를 다 가져오기
+# from module01 import *
+
+# print(add(3, 5))
+# print(sub(3, 5))
+
+# 3.
+import module01
+
+
+# 4.
+print(module01.PI)
+
+a = module01.Math()
+print(a.solve(2))
+
+print(module01.add(3, 64))
